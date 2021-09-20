cdad3adbec5fd3d31cb3b046e96cbcb860ca5011
Gleb Zinovyev <gleb.zinovyev@zerto.com>
2021-09-20 19:17:40 +0300

    .

---
+++ LeetCode/1396DesignUndergroundSystem/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._1396DesignUndergroundSystem
+{
+    public class UndergroundSystem
+    {
+        private Dictionary<int, TravelData> _dictionary = new Dictionary<int, TravelData>();
+
+        private Dictionary<string, Dictionary<string, AvgData>> _fromToTime =
+            new Dictionary<string, Dictionary<string, AvgData>>();
+
+        public UndergroundSystem()
+        {
+        }
+
+        public void CheckIn(int id, string stationName, int t)
+        {
+            _dictionary[id] = new TravelData() {InStation = stationName, InTime = t};
+        }
+
+        public void CheckOut(int id, string stationName, int t)
+        {
+            var travelData = _dictionary[id];
+            travelData.OutStation = stationName;
+            travelData.OutTime = t;
+
+
+            if (!_fromToTime.ContainsKey(travelData.InStation))
+            {
+                _fromToTime[travelData.InStation] = new Dictionary<string, AvgData>();
+            }
+
+            if (!_fromToTime[travelData.InStation].ContainsKey(travelData.OutStation))
+            {
+                _fromToTime[travelData.InStation][travelData.OutStation] = new AvgData();
+            }
+
+            _fromToTime[travelData.InStation][travelData.OutStation].Add(travelData);
+        }
+
+        public double GetAverageTime(string startStation, string endStation)
+        {
+            return _fromToTime[startStation][endStation].GetAVG();
+        }
+
+        private class TravelData
+        {
+            public string InStation { get; set; }
+            public int InTime { get; set; }
+            public string OutStation { get; set; }
+            public int OutTime { get; set; }
+        }
+
+        private class AvgData
+        {
+            private double totalTime;
+            private int totalTravels;
+
+            public void Add(TravelData travelData)
+            {
+                totalTime += travelData.OutTime - travelData.InTime;
+                totalTravels++;
+            }
+
+            public double GetAVG()
+            {
+                return totalTime / totalTravels;
+            }
+        }
+    }
+
+    /**
+     * Your UndergroundSystem object will be instantiated and called as such:
+     * UndergroundSystem obj = new UndergroundSystem();
+     * obj.CheckIn(id,stationName,t);
+     * obj.CheckOut(id,stationName,t);
+     * double param_3 = obj.GetAverageTime(startStation,endStation);
+     */
+}
---
+++ LeetCode/1396DesignUndergroundSystem/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LeetCode._1396DesignUndergroundSystem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            UndergroundSystem undergroundSystem = new UndergroundSystem();
+            undergroundSystem.CheckIn(45, "Leyton", 3);
+            undergroundSystem.CheckIn(32, "Paradise", 8);
+            undergroundSystem.CheckIn(27, "Leyton", 10);
+            undergroundSystem.CheckOut(45, "Waterloo", 15);  // Customer 45 "Leyton" -> "Waterloo" in 15-3 = 12
+            undergroundSystem.CheckOut(27, "Waterloo", 20);  // Customer 27 "Leyton" -> "Waterloo" in 20-10 = 10
+            undergroundSystem.CheckOut(32, "Cambridge", 22); // Customer 32 "Paradise" -> "Cambridge" in 22-8 = 14
+            undergroundSystem.GetAverageTime("Paradise", "Cambridge"); // return 14.00000. One trip "Paradise" -> "Cambridge", (14) / 1 = 14
+            undergroundSystem.GetAverageTime("Leyton", "Waterloo");    // return 11.00000. Two trips "Leyton" -> "Waterloo", (10 + 12) / 2 = 11
+            undergroundSystem.CheckIn(10, "Leyton", 24);
+            undergroundSystem.GetAverageTime("Leyton", "Waterloo");    // return 11.00000
+            undergroundSystem.CheckOut(10, "Waterloo", 38);  // Customer 10 "Leyton" -> "Waterloo" in 38-24 = 14
+            undergroundSystem.GetAverageTime("Leyton", "Waterloo");    // return 12.00000. Three trips "Leyton" -> "Waterloo", (10 + 12 + 14) / 3 = 12
+        }
+    }
+}
