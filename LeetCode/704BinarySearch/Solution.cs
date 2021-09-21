cbc8e6384ad6764456d77976f5d5a804449c96ba
Gleb Zinovyev <gleb.zinovyev@zerto.com>
2021-09-21 15:26:29 +0300

    704BinarySearch

---
+++ LeetCode/704BinarySearch/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._704BinarySearch
+{
+    public class Solution
+    {
+        private int midNumber;
+        private int midIndex;
+        public int Search(int[] nums, int target)
+        {
+            return _Search(nums, 0, nums.Length - 1, target);
+        }
+
+
+        private int _Search(int[] numbers, int left, int right, int target)
+        {
+            //Debug.WriteLine($"left:{ left}, right:{right}");
+
+            if (left > right)
+            {
+                return -1;
+            }
+
+            midIndex = ( right - left) /2 ;
+            midIndex += left;
+           // Debug.WriteLine($"midIndex:{midIndex}");
+
+            midNumber = numbers[midIndex];
+           // Debug.WriteLine($"midNumber:{ midNumber}");
+
+            if (midNumber == target)
+            {
+                return midIndex;
+            }
+
+            if (midNumber > target)
+            {
+                return _Search(numbers, left, midIndex-1, target);
+            }
+            else
+            {
+                return _Search(numbers, midIndex + 1, right, target);
+            }
+        }
+    }
+}
---
+++ LeetCode/704BinarySearch/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LeetCode._704BinarySearch
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution solution = new Solution();
+            solution.Search(new[] {-1, 0, 3, 5, 9, 12}, 2);
+        }
+    }
+}
