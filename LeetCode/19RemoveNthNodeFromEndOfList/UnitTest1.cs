0257f2d794543acfe27aec054b320e9a6235bccc
Gleb Zinovyev <gleb.zinovyev@zerto.com>
2021-10-02 16:09:36 +0300

    ...

---
+++ LeetCode/19RemoveNthNodeFromEndOfList/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode._19RemoveNthNodeFromEndOfList
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var items = new int[] {1};
+            ListNode list = new ListNode();
+            ListNode runner = list;
+
+            foreach (int item in items)
+            {
+                runner.next = new ListNode(item);
+                runner = runner.next;
+            }
+
+            var middleNode = new Solution().RemoveNthFromEnd(list.next, 1);
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode cureent = head;
+            List<ListNode> listNodes = new List<ListNode>();
+
+            while (cureent != null)
+            {
+                listNodes.Add(cureent);
+                cureent = cureent.next;
+            }
+
+
+            int removeIndex = listNodes.Count - n;
+            if (1 < n && n < listNodes.Count)
+            {
+                listNodes[removeIndex - 1].next = listNodes[removeIndex + 1];
+                listNodes[removeIndex].next = null;
+            }
+            else if (n == 1)
+            {
+                if (listNodes.Count == 1)
+                {
+                    return null;
+                }
+                listNodes[removeIndex - 1].next = null;
+            }
+            else //n == listNodes.Count
+            {
+                head = listNodes[removeIndex].next;
+                listNodes[removeIndex].next = null;
+            }
+
+            return head;
+        }
+    }
+}
---
+++ LeetCode/3.LongestSubstringWithoutRepeatingCharacters/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode._3.LongestSubstringWithoutRepeatingCharacters
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var lengthOfLongestSubstring = new Solution().LengthOfLongestSubstring("dvdf");
+        }
+    }
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            Dictionary<char, int> list = new Dictionary<char, int>();
+            int counter = 0;
+            int max = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+
+                if (list.ContainsKey(c))
+                {
+                    if (counter > max)
+                    {
+                        max = counter;
+                    }
+                  
+                    i = list[c];
+                    list.Clear();
+                    counter = 0;
+                }
+                else
+                {
+                    list.Add(c, i);
+                    counter++;
+                }
+             
+            }
+            if (counter > max)
+            {
+                max = counter;
+            }
+            return max;
+        }
+    }
+}
