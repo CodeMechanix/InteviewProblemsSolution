eb836db1a5c465ebda111813239a1f55ac77e508
Gleb Zinovyev <gleb.zinovyev@zerto.com>
2021-10-23 09:08:32 +0300

    save

--- Other/CheckPoint/UnitTest1.cs
+++ Other/CheckPoint/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace Other.CheckPoint
 {
@@ -9,6 +10,40 @@ namespace Other.CheckPoint
         [TestMethod]
         public void TestMethod1()
         {
+            var solution = new Solution().solution(new [] { -1, -3 });
+        }
+    }
+    // write your code in C# 6.0 with .NET 4.5 (Mono)
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int N = A.Length;
+            int positiveCount = 0;
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            foreach (int num in A)
+            {
+                if (num > 0)
+                {
+                    positiveCount++;
+                    dictionary[num] = num;
+                }
+              
+            }
+
+
+            for (int i = 1; i <= positiveCount+1; i++)
+            {
+                if (!dictionary.ContainsKey(i))
+                {
+                    return i;
+                }
+            }
+
+
+            return -1;
+
         }
     }
 }
