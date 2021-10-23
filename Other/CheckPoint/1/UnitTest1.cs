2f1935ba85f0ef1565ebdac025260283e9686997
Gleb Zinovyev <gleb.zinovyev@zerto.com>
2021-10-23 09:38:12 +0300

    save 1

--- Other/CheckPoint/1/UnitTest1.cs
+++ Other/CheckPoint/1/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.CodeDom;
 
 namespace Other.CheckPoint._1
 {
@@ -9,6 +10,50 @@ namespace Other.CheckPoint._1
         [TestMethod]
         public void TestMethod1()
         {
+//            var S = "id,name,age,act.,room,dep.\n1,Jack,68,T,13,8\n17,Betty,28,F,15,7";
+            var S = "area,land\n3722,CN\n6612,RU\n3855,CA\n3797,USA";
+            var C = "area";
+            var solution = new Solution().solution(S,C);
+        }
+        class Solution
+        {
+            public int solution(string S, string C)
+            {
+                string[] lines = S.Split(new []{ '\n' });
+
+
+                string[] conNames = lines[0].Split(new []{','});
+
+                int conNum = 0;
+                for (int i = 0; i < conNames.Length; i++)
+                {
+                    if (conNames[i].Equals(C))
+                    {
+                        conNum = i;
+                        break;
+                    }
+                }
+
+                int max = int.MinValue;
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    var line = lines[i];
+                    var cellString = line.Split(new []{','})[conNum];
+                    var cellNum = Int32.Parse(cellString);
+
+                    if (cellNum > max)
+                    {
+                        max = cellNum;
+                    }
+                }
+
+
+
+
+
+                return max;
+            }
         }
     }
 }
