2ba64c1de3d8875f3e8cee71a02aee09214dbe56
Gleb Zinovyev <gleb.zinovyev@zerto.com>
2021-10-31 07:13:36 +0200

    edit

---
+++ Codility/L1/BinaryGap/UnitTest1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Codility.L1.BinaryGap
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var solution = new Solution().solution(32);
+        }
+    }
+
+    class Solution
+    {
+        private IState State { get; set; }
+        private int Max { get; set; }
+        public Solution()
+        {
+            State = new InitialSate(this);
+        }
+
+        public int solution(int N)
+        {
+            string binary = IntToBinary(N);
+
+            foreach (char c in binary)
+            {
+                State.Handle(c);
+            }
+
+            return Max;
+        }
+
+        private string IntToBinary(int num)
+        {
+            return Convert.ToString(num, 2);
+        }
+
+
+         interface IState
+        {
+            void Handle(char num);
+        }
+
+        class InitialSate : IState
+        {
+            private readonly Solution _context;
+
+            public InitialSate(Solution context)
+            {
+                _context = context;
+            }
+            public void Handle(char num)
+            {
+                if (num == '1')
+                {
+                    _context.State = new CountState(_context);
+                }
+            }
+        }
+
+        class CountState : IState
+        {
+            private readonly Solution _context;
+            private int _currentCounter;
+            public CountState(Solution context)
+            {
+                _context = context;
+            }
+
+            public void Handle(char num)
+            {
+                if (num == '0')
+                {
+                    _currentCounter++;
+                }
+                else
+                {
+                    if (_context.Max < _currentCounter)
+                    {
+                        _context.Max = _currentCounter;
+                    }
+
+                    _currentCounter = 0;
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ Codility/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Codility")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Codility")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db1db675-a919-41f5-a662-b0daa60a89ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
