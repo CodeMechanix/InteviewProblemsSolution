9e6b4a83130530e08ada65f19df65ef5f27401a7
Gleb Zinovyev <gleb.zinovyev@zerto.com>
2021-11-02 07:56:21 +0200

    OddOccurrencesInArray

---
+++ Codility/L2/OddOccurrencesInArray/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility.L2.OddOccurrencesInArray
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var solution = new Solution().solution(new []{9,3,9,3,9,7,9});
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            foreach (int i in A)
+            {
+                if (dictionary.ContainsKey(i))
+                {
+                    dictionary.Remove(i);
+                }
+                else
+                {
+                    dictionary.Add(i, 0);
+                }
+            }
+
+            return dictionary.Keys.First();
+        }
+    }
+}
