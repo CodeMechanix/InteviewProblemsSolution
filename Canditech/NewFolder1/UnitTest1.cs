9913738b7232f708a0d62b894de6609f47deba17
Gleb Zinovyev <gleb.zinovyev@zerto.com>
2021-11-04 07:37:00 +0200

    save canditech

---
+++ Canditech/NewFolder1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Codility.NewFolder1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var isPalindrome = Solution.IsPalindrome("madam");
+        }
+    }
+    class Solution
+    {
+        public static bool IsPalindrome(string text)
+        {
+            return Check(text, 0, text.Length-1);
+        }
+
+        private static bool Check(string text, int left, int right)
+        {
+            if (left > right)
+            {
+                return true;
+            }
+
+            char leftChar = text[left];
+            char rightChar = text[right];
+
+            if (leftChar == rightChar)
+            {
+                return Check(text, left + 1, right - 1);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Canditech/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Canditech")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Canditech")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b000af0-3dbf-4cd2-a7d3-55d31b409aa7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
