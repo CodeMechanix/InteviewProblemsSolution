66a066a844d32d43a128e410c72df2ae51ac10a1
Gleb Zinovyev <gleb.zinovyev@hpe.com>
2022-05-17 07:19:59 +0300

    SupermanTest

---
+++ Educative/multithreading/SupermanTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Educative.multithreading
+{
+    [TestClass]
+    public class SupermanTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    class Superman
+    {
+        private static volatile Superman _instance;
+        private static readonly object Lock = new object();
+
+        public static Superman Instance()
+        {
+            if (_instance is null)
+            {
+                lock (Lock)
+                {
+                    if (_instance is null)
+                    {
+                        _instance = new Superman();
+                    }
+                }
+            }
+
+
+            return _instance;
+        }
+
+        private Superman()
+        {
+        }
+    }
+}
