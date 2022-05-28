54e01c0ad1cc3db567fff5fd1e6a348739aff19a
Gleb Zinovyev <gleb.zinovyev@hpe.com>
2022-05-28 18:00:00 +0300

    MergeSort in plase implementation

--- CrackingCode/Sorting/MergeSortTest.cs
+++ CrackingCode/Sorting/MergeSortTest.cs
@@ -10,8 +10,10 @@ namespace CrackingCode.Sorting
         [TestMethod]
         public void TestMethod1()
         {
-            MergeSort mergeSort = new MergeSort();
-            List<int> sort = mergeSort.Sort(new[] { 3, 7, 5, 9, 1, 3, 6 });
+            // var mergeSort = new MergeSort();
+            // var sort = mergeSort.Sort(new[] { 3, 7, 5, 9, 1, 6 });
+            var mergeSortII = new MergeSortII();
+            mergeSortII.Sort(new List<int>() { 3, 7, 5, 9, 1, 6 });
         }
     }
 
@@ -64,5 +66,78 @@ namespace CrackingCode.Sorting
         // {
         //
         // }
+    }
+
+    class MergeSortII
+    {
+        public void Sort(List<int> ints)
+        {
+            Split(ints, 0, ints.Count - 1);
+        }
+
+        private void Split(List<int> list, int left, int right)
+        {
+            int length = right - left;
+
+            if (length <= 0)
+            {
+                return;
+            }
+
+            int mid = length / 2 + left;
+
+            Split(list, left, mid);
+
+            Split(list, mid + 1, right);
+
+
+            Merge(list, left, mid, mid + 1, right);
+        }
+
+        private void Merge(List<int> list, int leftStart, int leftEnd, int rightStart, int rightEnd)
+        {
+            List<int> left = new List<int>();
+
+            for (int i = leftStart; i <= leftEnd; i++)
+            {
+                left.Add(list[i]);
+            }
+
+            List<int> right = new List<int>();
+
+            for (int i = rightStart; i <= rightEnd; i++)
+            {
+                right.Add(list[i]);
+            }
+
+            int indexMain = leftStart;
+
+            while (left.Count > 0 && right.Count > 0)
+            {
+                int next;
+                if (left[0] < right[0])
+                {
+                    next = left[0];
+                    left.RemoveAt(0);
+                }
+                else
+                {
+                    next = right[0];
+                    right.RemoveAt(0);
+                }
+                list[indexMain] = next;
+                indexMain++;
+            }
+
+            List<int> leftOvers = left.Count > 0 ? left : right;
+
+            while (leftOvers.Count > 0)
+            {
+                list[indexMain] = leftOvers[0];
+                leftOvers.RemoveAt(0);
+                indexMain++;
+            }
+
+        }
     }
 }
