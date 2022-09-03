af845d98df689c8c285742e7000132acab1f218c
ZERTO\gleb.zinovyev <gleb.zinovyev@zerto.com>
2022-09-03 19:55:07 +0300

    AsynchronousToSynchronousProblem

---
+++ Educative/multithreading/AsynchronousToSynchronousProblem.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Educative.multithreading
+{
+    [TestClass]
+    public class AsynchronousToSynchronousProblem
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            ManualResetEventSlim eventSlim = new ManualResetEventSlim();
+
+            Executor executor = new Executor();
+            executor.asynchronousExecution(() =>
+            {
+                Debug.WriteLine("I am done");
+                eventSlim.Set();
+            });
+
+            eventSlim.Wait();
+
+            Debug.WriteLine("main thread exiting...");
+
+
+
+        }
+    }
+
+    
+
+    class Executor
+    {
+        public void asynchronousExecution(Action callback)
+        {
+            Thread t = new Thread(() =>
+            {
+                // Do some useful work
+                try
+                {
+                    Thread.Sleep(5000);
+                }
+                catch (Exception ie)
+                {
+                }
+
+                callback();
+            });
+            t.Start();
+        }
+    }
+}
