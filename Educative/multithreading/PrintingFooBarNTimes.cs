35d5fed6b4826ca4320e79f58bd5ca1fc4837912
ZERTO\gleb.zinovyev <gleb.zinovyev@zerto.com>
2022-09-09 18:59:38 +0300

    add PrintingZeroEvenOdd

---
+++ Educative/multithreading/PrintingFooBarNTimes.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Educative.multithreading
+{
+    [TestClass]
+    public class PrintingFooBarNTimes
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var printFooBar = new PrintFooBar(10);
+            var task = Task.Run(() => printFooBar.PrintBar());
+            Task run = Task.Run(() => { printFooBar.PrintFoo(); });
+
+            Task.WaitAll(new Task[]{task, run});
+
+        }
+    }
+
+    class PrintFooBar
+    {
+        private readonly int _n;
+        private AutoResetEvent _eventFoo = new AutoResetEvent(true);
+        private AutoResetEvent _eventBar = new AutoResetEvent(false);
+
+        public PrintFooBar(int n)
+        {
+            _n = n;
+        }
+        public void PrintFoo()
+        {
+          
+            for (int i = 1; i <= _n; i++)
+            {
+                _eventFoo.WaitOne();
+                Debug.WriteLine("Foo");
+                _eventBar.Set();
+            }
+        }
+        public void PrintBar()
+        {
+          
+            for (int i = 1; i <= _n; i++)
+            {
+                _eventBar.WaitOne();
+               Debug.WriteLine("Bar");
+               _eventFoo.Set();
+            }
+        }
+    }
+
+}
---
+++ Educative/multithreading/PrintingZeroEvenOdd.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Educative.multithreading
+{
+    [TestClass]
+    public class PrintingZeroEvenOdd
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var printNumberSeries = new PrintNumberSeries(10);
+            printNumberSeries.Run();
+        }
+    }
+
+    class PrintNumberSeries
+    {
+        private int _n;
+        private int _i;
+
+        AutoResetEvent _eventZero = new AutoResetEvent(true);
+
+        // AutoResetEvent _event = new AutoResetEvent(false);
+        private object _lock = new object();
+
+        public PrintNumberSeries(int n)
+        {
+            this._n = n;
+        }
+
+        public void Run()
+        {
+            var task = Task.Run(PrintZero);
+            var run = Task.Run(PrintEven);
+            var task1 = Task.Run(PrintOdd);
+            Task.WaitAll(new[] { task1, task, run });
+        }
+
+
+        public void PrintZero()
+        {
+            while (Working())
+            {
+                _eventZero.WaitOne();
+
+                Debug.WriteLine("0");
+                _i++;
+                lock (_lock)
+                {
+                    Monitor.PulseAll(_lock);
+                }
+            }
+        }
+
+        public void PrintOdd()
+        {
+            lock (_lock)
+            {
+                Monitor.Wait(_lock);
+                while (Working())
+                {
+                    while (_i % 2 == 1)
+                    {
+                        Monitor.Wait(_lock);
+                    }
+
+                    Debug.WriteLine(_i);
+
+                   
+                    _eventZero.Set();
+                    Monitor.Wait(_lock);
+                }
+            }
+        }
+
+        public void PrintEven()
+        {
+            lock (_lock)
+            {
+                Monitor.Wait(_lock);
+                while (Working())
+                {
+                    while (_i % 2 == 0)
+                    {
+                        Monitor.Wait(_lock);
+                    }
+
+                    Debug.WriteLine(_i);
+
+
+                    _eventZero.Set();
+                    Monitor.Wait(_lock);
+                }
+            }
+        }
+
+        private bool Working()
+        {
+            lock (_lock)
+            {
+                return _i != _n;
+            }
+        }
+    }
+}
