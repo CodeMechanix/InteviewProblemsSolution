71e7d50fffb2638c4e365187c8b25b4f8b394be5
GZ <GZ@GZ_PC>
2024-07-20 14:34:32 +0300

    add PeakBlocks

---
+++ Codility/L10/Flags.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Codility.L10
+{
+    [TestClass]
+    public class Flags
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            int[] arr = { 1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };
+            //int[] peaks = new Peaks(arr).Result();
+            //int[] peakDistances = new Distances(peaks).Result();
+            //int[] ints = new Multiplicators(2, 9).Result();
+
+            int maxFlags = new MaxFlags(arr).Result();
+        }
+
+        private class MaxFlags
+        {
+            private readonly int[] _arr;
+
+            public MaxFlags(int[] arr)
+            {
+                _arr = arr;
+            }
+
+            public int Result()
+            {
+                int[] peakList = new Peaks(_arr).Result();
+                if (peakList.Length == 0)
+                {
+                    return 0;
+                }
+                if (peakList.Length == 1)
+                {
+                    return 1;
+                }
+
+                var maxFalgs = 1;
+                var nextEnd = 0;
+
+                for (int k = 2; k < peakList.Length + 1; k++)
+                {
+                    var flags = 1;
+                    var start = 0;
+                    var shouldSetNextEnd = true;
+                   
+                    var end = nextEnd;
+                    while (
+                        flags < k &&
+                        start < peakList.Length &&
+                        end < peakList.Length)
+                    {
+                        var distanceToNextPeak = peakList[end] - peakList[start];
+
+                        if (distanceToNextPeak >= k)
+                        {
+                            if (shouldSetNextEnd)
+                            {
+                                nextEnd = end;
+                                shouldSetNextEnd = false;
+                            }
+                            flags++;
+                            start = end;
+                        }
+                        else
+                        {
+                            end++;
+                        }
+                        
+                    }
+
+                    if (maxFalgs < flags)
+                    {
+                        maxFalgs = flags;
+                    }
+                    else if(maxFalgs == flags) 
+                    {
+                        break;
+                    }
+                }
+
+                return maxFalgs;
+            }
+        }
+
+        private class Peaks
+        {
+            private readonly int[] _values;
+
+            public Peaks(int[] values)
+            {
+                _values = values;
+            }
+
+            public int[] Result()
+            {
+                List<int> result = new List<int>();
+
+                for (int i = 1; i < _values.Length - 1; i++)
+                {
+                    int left = _values[i - 1];
+                    int right = _values[i + 1];
+                    int center = _values[i];
+
+                    if (left < center && center > right)
+                    {
+                        result.Add(i);
+                        i++;
+                    };
+                }
+
+                return result.ToArray();
+            }
+
+        }
+    }
+}
---
+++ Codility/L10/Peaks.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Codility.L10
+{
+    [TestClass]
+    public class Peaks
+    {
+
+        [TestMethod]
+        public void Test()
+        {
+            var a = new int[] { 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };
+            //var a = new int[] { 0, 1, 0, 0, 1, 0, 0, 1, 0 };
+
+            var peakBlocks = new PeakBlocks(a);
+            var max = peakBlocks.MaxBlocks();
+            //bool v = peakBlocks.CanBeDevidedToBlocks(1);
+            // v = peakBlocks.CanBeDevidedToBlocks(2);
+            // v = peakBlocks.CanBeDevidedToBlocks(3);
+            // v = peakBlocks.CanBeDevidedToBlocks(4);
+            // v = peakBlocks.CanBeDevidedToBlocks(5);
+        }
+
+        private class PeakBlocks
+        {
+            private readonly bool[] _peakArray;
+            private readonly DevisorList _devisorPairList;
+            public PeakBlocks(int[] altitudes)
+            {
+                _peakArray = new PeaksArray(altitudes).Result();
+                _devisorPairList = new DevisorList(altitudes.Length);
+                _devisorPairList.GenerateDevisors();
+            }
+
+            public int MaxBlocks()
+            {
+                int maxBlocks = 0;
+
+                var orderedEnumerable = _devisorPairList
+                    .SelectMany(pair => new int[] { pair.Item1, pair.Item2 })
+                    .Distinct()
+                    .OrderByDescending(i => i)
+                    .Reverse()
+                    .ToArray();
+
+                for (int i = 0; i < orderedEnumerable.Length; i++)
+                {
+                    if (CanBeDevidedToBlocks(orderedEnumerable[i]))
+                    {
+                        maxBlocks= orderedEnumerable[i];
+                    }
+                }
+
+                return maxBlocks;
+            }
+
+            public bool CanBeDevidedToBlocks(int numOfBlocks)
+            {
+                if (_peakArray.Length < numOfBlocks)
+                {
+                    return false;
+                }
+
+                var blockLength = _peakArray.Length / numOfBlocks;
+                var index = 0;
+                while (index < _peakArray.Length)
+                {
+                    if (BlockContainsPeak(blockLength, index))
+                    {
+                        index += blockLength;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+
+            private bool BlockContainsPeak(int blockLength, int startIndex)
+            {
+                int index = startIndex;
+                while (index < _peakArray.Length && index < startIndex + blockLength)
+                {
+                    if (_peakArray[index])
+                    {
+                        return true;
+                    }
+                    index++;
+                }
+                return false;
+            }
+        }
+        private class PeaksArray
+        {
+            private readonly int[] _array;
+
+            public PeaksArray(int[] array)
+            {
+                _array = array;
+            }
+
+            public bool[] Result()
+            {
+                var peaks = new bool[_array.Length];
+
+                for (int i = 1; i < _array.Length - 1; i++)
+                {
+                    if (_array[i - 1] < _array[i] && _array[i] > _array[i + 1])
+                    {
+                        peaks[i] = true;
+                    }
+                }
+                return peaks;
+            }
+        }
+    }
+
+
+}
