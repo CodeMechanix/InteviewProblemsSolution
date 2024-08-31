5a7cd1fc16492875501a527e85404c634865cd71
GZ <GZ@GZ_PC>
2024-08-31 18:49:35 +0300

    13

---
+++ Codility/L13/FibFrog.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codility.L13
+{
+    class FibFrog
+    {
+        public class Solution
+        {
+
+
+            public int solution(int[] A)
+            {
+
+                var fibLeafTree = new FibLeafTree(A);
+                var fibLeafTree_BFS = new FibLeafTree_BFS(fibLeafTree);
+                return fibLeafTree_BFS.StepsCount(A.Length);
+
+            }
+
+        }
+
+        class FibLeafTree_BFS
+        {
+            private readonly FibLeafTree _tree;
+
+            public FibLeafTree_BFS(FibLeafTree tree)
+            {
+                _tree = tree;
+            }
+
+            public int StepsCount(int target)
+            {
+                var queue = new Queue<(int position, int spetCount)>();
+                var visited = new HashSet<int>();
+                queue.Enqueue((-1, 0));
+
+
+                while (queue.Count > 0)
+                {
+                    (int position, int spetCount) = queue.Dequeue();
+
+
+                    if (position == target)
+                    {
+                        return spetCount;
+                    }
+
+                    if (visited.Contains(position))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        visited.Add(position);
+                    }
+
+
+                    int[] childs = _tree.GetChildFibPositions(position);
+
+                    foreach (var item in childs)
+                    {
+                        queue.Enqueue((item, spetCount + 1));
+                    }
+                }
+
+                return -1;
+
+            }
+
+        }
+
+        class FibLeafTree
+        {
+            private readonly int[] _a;
+            private int[] _fibArray;
+
+            public FibLeafTree(int[] A)
+            {
+                _a = A;
+                _fibArray = new FibonacciDynamic().FibSequence(A.Length + 3);
+
+            }
+
+            public int[] GetChildFibPositions(int position)
+            {
+                var childPositions = new List<int>();
+
+                for (int i = 2; i < _fibArray.Length; i++)
+                {
+                    int fibNumber = _fibArray[i];
+                    var nextPosition = fibNumber + position;
+
+                    if (nextPosition > _a.Length)
+                        break;
+
+                    if (nextPosition == _a.Length || _a[nextPosition] == 1)
+                    {
+                        childPositions.Add(nextPosition);
+                    }
+                }
+
+                return childPositions.ToArray();
+            }
+        }
+
+        class FibonacciDynamic
+        {
+
+            public FibonacciDynamic()
+            {
+            }
+
+            public int FibOf(int fibIndex)
+            {
+                var storage = new int[3];
+                storage[1] = 1;
+
+                int index = 2;
+                while (index < fibIndex)
+                {
+                    storage[2] = storage[0] + storage[1];
+                    Array.Copy(storage, 1, storage, 0, storage.Length - 1);
+                    index++;
+                }
+
+
+                return storage[2];
+            }
+
+            public int[] FibSequence(int maxLimit)
+            {
+                var storage = new List<int> { 0, 1 };
+
+                int index = 2;
+                while (index < maxLimit)
+                {
+                    int nextFibValue = storage[index - 1] + storage[index - 2];
+
+                    if (nextFibValue > maxLimit)
+                    {
+                        break;
+                    }
+
+                    storage.Add(nextFibValue);
+                    index++;
+                }
+
+                return storage.ToArray();
+            }
+
+
+        }
+    }
+
+
+}
---
+++ Codility/L13/FibonacciNumbersTest.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Codility.L13
+{
+    [TestClass]
+    public class FibonacciNumbersTest
+    {
+        [TestMethod]
+        public void FibFrogTest()
+        {
+            int v = new FibFrog.Solution()
+                .solution(new int[] {0 });
+        }
+
+        [TestMethod]
+        public void LadderTest()
+        {
+            long v = new LadderSolution.LadderWays().NumberOfDifferentWaysOfClimbingLadder(5);
+        }
+    }
+
+    class LadderSolution
+    {
+        class Solution { 
+            public int[] solution(int[] A, int[] B)
+            {
+
+                var ladderWays = new LadderWays();
+
+                var result = new int[A.Length];
+                for (int i = 0; i < A.Length; i++)
+                {
+                    var v = ladderWays.NumberOfDifferentWaysOfClimbingLadder(A[i]);
+                    var v1 = (int) Math.Pow(2, B[i]);
+                    result[i] =  v % v1;
+                }
+                return result;
+            } 
+        }
+
+        public class LadderWays
+        {
+            public int NumberOfDifferentWaysOfClimbingLadder(int stepsCount)
+            {
+                //create stuck and save parrent's index in child
+                //when no more child, pop the top o the stack, update the parent way-count, update is-visited
+                
+                var isVisited = new bool[stepsCount + 1];
+                var wayCount = new int[stepsCount +1];
+                var parrents = new int[stepsCount +1];
+                var stack = new Stack<int>();
+                stack.Push(0);
+                wayCount[stepsCount] = 1;
+
+                while (stack.Count > 0) {
+
+                    var current = stack.Peek();
+
+                    if (isVisited[current])
+                    {
+                        var pop = stack.Pop();
+                        if (pop == 0) break;
+
+
+                        var parrent = parrents[pop];
+                        wayCount[parrent] = wayCount[parrent] + wayCount[pop];
+                    }
+                    else {
+
+                        isVisited[current] = true;
+                        var childs = GetGilds(current, stepsCount);
+                        foreach (var item in childs)
+                        {
+                            parrents[item] = current;
+                            stack.Push(item);
+                        }
+                    }
+                }
+
+                return wayCount[0];
+            }
+
+            private int[] GetGilds(int parrentIndex, int total)
+            {
+                var childs = new List<int>();
+
+                var childTemp = new int[] { parrentIndex + 1, parrentIndex + 2 };
+
+                foreach (var item in childTemp)
+                {
+                    if (item <= total)
+                    {
+                        childs.Add(item);
+                    }
+                }
+
+                return childs.ToArray();
+            }
+        }
+        
+    }
+}
