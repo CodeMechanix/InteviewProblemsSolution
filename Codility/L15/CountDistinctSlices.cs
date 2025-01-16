a4ceb6d932a2d5b85ee63f6fa288b86aba733851
gz <gz@DESKTOP-P0HP5PH>
2025-01-16 07:16:51 +0200

    CountDistinctSlices

---
+++ Codility/L15/CountDistinctSlices.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Codility.L15
+{
+    [TestClass]
+    public class CountDistinctSlices
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+           var v = new Solution().solution(0,new int[] { 3, 4, 5, 5, 2 });
+        }
+
+        class Solution
+        {
+            public int solution(int M, int[] A)
+            {
+                int counter = 0;
+
+                for (int i = 0; i < A.Length; i++) {
+
+                    HashSet<int> visited = new HashSet<int>();
+                    int runner = i;
+
+                    while (runner < A.Length && !visited.Contains(A[runner]))
+                    {
+                        visited.Add(A[runner]);
+                        runner++;
+                    }
+
+                    counter += RecursiveSum(runner - i );
+                    i = runner - 1;
+                }
+                
+                return counter;
+            }
+
+            int RecursiveSum(int val)
+            {
+                if (val == 0) return 0;
+                return RecursiveSum(val - 1) + val;
+            }
+        }
+
+
+    }
+}
