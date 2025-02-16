e91c4b4c854aeacff033be2aebd549fa0dcdd7ee
admin <admin@MACHINE>
2025-02-16 06:48:40 +0200

    add StringPermutation

---
+++ CodingPatterns/CodingPatterns/SlidingWindow/ReplacingOnes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace CodingPatterns.SlidingWindow
+{
+    [TestClass]
+    public class ReplacingOnes_Test
+    {
+        [TestMethod]
+        [DataRow(new []{ 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1 }, 2, 6)]
+        [DataRow(new []{ 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1 }, 3, 9)]
+        public void Test(int[] ints, int k, int maxLength)
+        {
+            ReplacingOnes.findLength(ints, k)
+                .ShouldBe(maxLength);
+        }
+    }
+    class ReplacingOnes
+    {
+        public static int findLength(int[] arr, int k)
+        {
+            int maxLength = 0;
+            int start = 0;
+            int kCount = 0;
+            for (int end = 0; end < arr.Length; end++)
+            {
+                int current = arr[end];
+
+                if (current == 0)
+                {
+                    kCount++;
+                }
+
+                while (kCount > k)
+                {
+                    var firstValue = arr[start];
+                    if (firstValue == 0)
+                    {
+                        kCount--;
+                    }
+                    start++;
+                }
+
+                maxLength = Math.Max(maxLength, end - start +1);
+
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ CodingPatterns/CodingPatterns/SlidingWindow/StringPermutation.cs
@@ -0,0 +1,96 @@
+﻿using Shouldly;
+
+namespace CodingPatterns.SlidingWindow
+{
+    [TestClass]
+    public class StringPermutation_Test
+    {
+        [TestMethod]
+        [DataRow("oidbcaf", "abc", true)]
+        [DataRow("odicf", "dc", false)]
+        [DataRow("bcdxabcdy", "bcdyabcdx", true)]
+        [DataRow("abbbaacb", "abc", true)]
+        public void Test(string str, string pattern, bool result)
+        {
+            StringPermutation.findPermutation(str, pattern)
+                .ShouldBe(result);
+        }
+    }
+
+    class StringPermutation
+    {
+        public static bool findPermutation(string str, string pattern)
+        {
+            int start = 0;
+            var patternDic = PrepareDic(pattern);
+            var tmpDic = new Dictionary<char, int>();
+            for (int end = 0; end < str.Length; end++)
+            {
+                char c = str[end];
+                if (patternDic.ContainsKey(c))
+                {
+                    Incement( tmpDic, c );
+                }
+                else
+                {
+                    start = end +1;
+                    tmpDic = new Dictionary<char, int>();
+                    continue;
+                }
+
+                while (tmpDic.ContainsKey(c) && tmpDic[c] > patternDic[c])
+                {
+                    Decement( tmpDic, str[start]);
+                    start++;
+                }
+
+                if ((end - start + 1) == pattern.Length)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static void Incement(Dictionary<char, int> dic, char key)
+        {
+            if (dic.ContainsKey(key))
+            {
+                dic[key]++;
+            }
+            else
+            {
+                dic[key] = 1;
+            }
+        }
+
+        private static void Decement(Dictionary<char, int> dic, char key)
+        {
+            if (dic.ContainsKey(key))
+            {
+                dic[key]--;
+            }           
+        }
+        private static Dictionary<char, int> PrepareDic(string pattern)
+        {
+            var dic = new Dictionary<char, int>();
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (dic.ContainsKey(pattern[i]))
+                {
+                    dic[pattern[i]]++;
+                }
+                else
+                {
+                    dic[pattern[i]] = 1;
+                }
+            }
+            return dic;
+        }
+
+        
+
+    }
+}
+
