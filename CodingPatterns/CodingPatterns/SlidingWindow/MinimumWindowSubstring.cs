3b63e31cbac0971a7325865c867d42dea591e385
admin <admin@MACHINE>
2025-02-22 16:25:53 +0200

    MinimumWindowSubstring

---
+++ CodingPatterns/CodingPatterns/SlidingWindow/MinimumWindowSubstring.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace CodingPatterns.SlidingWindow
+{
+    class MinimumWindowSubstring
+    {
+        public static String findSubstring(String str, String pattern)
+        {
+            var map = new Dictionary<char, int>();
+            foreach (char c in pattern) {
+            
+                map[c] = map.ContainsKey(c) ? map[c] + 1 : 1;
+            }
+
+            string minSubstring = string.Empty;
+            int start = 0;
+            int charMatchCount = 0;
+
+            for (int end = 1; end <= str.Length; end++)
+            {
+                var currentChar = str[end-1];
+
+                if (map.ContainsKey(currentChar))
+                {
+                    map[currentChar]--;
+                    if (map[currentChar] == 0)
+                    {
+                        charMatchCount++;
+                    }
+                }
+
+                while (charMatchCount == map.Count)
+                {
+                    if (minSubstring.Length > end - start - 1 || minSubstring.Equals(string.Empty))
+                    {
+                        minSubstring = str.Substring(start, end - start );
+
+                        if(minSubstring.Length == pattern.Length) return minSubstring;
+                    }
+
+                    var firstInWindow = str[start];
+                    if (map.ContainsKey(firstInWindow))
+                    {
+                        if (map[firstInWindow] == 0)
+                        {
+                            charMatchCount--;
+                        }
+                        map[firstInWindow]++;
+                    }
+                    start++;
+                }
+
+                
+            }
+            return minSubstring;
+        }
+    }
+    [TestClass]
+    public class MinimumWindowSubstring_Test
+    {
+        [TestMethod]
+        [DataRow("aabdec", "abc", "abdec")]
+        [DataRow("abdabca", "abc", "abc")]
+        [DataRow("adcad", "abc", "")]
+        public void Test(String str, String pattern, String expected)
+        {
+            MinimumWindowSubstring.findSubstring(str, pattern)
+                .ShouldBe(expected);
+        }
+    }
+}
---
+++ CodingPatterns/CodingPatterns/SlidingWindow/StringAnagrams.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace CodingPatterns.SlidingWindow
+{
+    class StringAnagrams
+    {
+        public static List<int> findStringAnagrams(String str, String pattern)
+        {
+            //init HashMap
+            var map = new Dictionary<char, int>();
+            foreach (var item in pattern)
+            {
+                if (map.ContainsKey(item))
+                {
+                    map[item]++;
+                }
+                else {
+                    map[item] = 1;
+                }
+            }
+
+            //use window
+            List<int> resultIndices = new List<int>();
+            int start = 0;
+            int matchCount =0;
+            for (int end = 1; end <= str.Length; end++)
+            {
+                //grow
+                var currentLetter = str[end-1];
+
+                if (map.ContainsKey(currentLetter))
+                {
+                    map[currentLetter]--;
+                    matchCount++;
+                }
+                else
+                {
+                    start = end-1;
+                    matchCount = 0;
+                    continue;
+                }
+
+
+                //shrink
+                while (map.ContainsKey(currentLetter) && map[currentLetter]<0)
+                {
+                    var firstLetter = str[start];
+                    map[firstLetter]++;
+                    matchCount--;
+                    start++;
+                }
+
+                if (matchCount == pattern.Length) { 
+                    resultIndices.Add(start);
+                }
+            }
+
+
+
+            // TODO: Write your code here
+            return resultIndices;
+        }
+    }
+
+    [TestClass]
+    public class StringAnagrams_Test
+    {
+        [TestMethod]
+        //[DataRow("ppqp", "pq", new[] {1,2})]
+        [DataRow("abbcabc", "abc", new[] { 2, 3, 4 })]
+        public void Test(String str, String pattern, int[] results)
+        {
+            StringAnagrams.findStringAnagrams(str, pattern)
+                .ShouldBe(results);
+        }
+    }
+}
