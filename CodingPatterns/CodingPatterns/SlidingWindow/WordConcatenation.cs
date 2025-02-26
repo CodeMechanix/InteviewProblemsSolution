546c7ac9bdd575a9118956b4e569bbaa36f1b7f2
admin <admin@MACHINE>
2025-02-26 08:01:13 +0200

    PairWithTargetSum

---
+++ CodingPatterns/CodingPatterns/SlidingWindow/WordConcatenation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace CodingPatterns.SlidingWindow
+{
+    class WordConcatenation
+    {
+        public static List<int> findWordConcatenation(String str, String[] words)
+        {
+            var resultIndices = new List<int>();
+            var map = new Dictionary<String, int>();
+            // build hashMap
+            foreach (var word in words) 
+            {
+                map[word] = map.ContainsKey(word) ? map[word] + 1 : 1;
+            }
+            var stepLength = words.First().Length;
+            var matchCount = 0;
+            var start = 0;
+            for (int end = 3; end <= str.Length; end+= stepLength)
+            {
+                var currentWord = str.Substring(end - stepLength, stepLength);
+
+                if (map.ContainsKey(currentWord))
+                {
+                    if (map[currentWord] > 0)
+                    {
+                        matchCount++;
+                    }
+                    map[currentWord]--;
+                }
+
+                while (matchCount == words.Length)
+                {
+                    if ((end - start) == words.Length * stepLength)
+                    {
+                        resultIndices.Add(start);
+                    }
+
+                    var leftWord = str.Substring(start , stepLength);
+                    if (map.ContainsKey(leftWord))
+                    {
+                        map[leftWord]++;
+                        if (map[leftWord] > 0)
+                        {
+                            matchCount--;
+                        }
+                    }
+
+                    start += stepLength;
+                }
+
+            }
+
+            return resultIndices;
+        }
+    }
+
+    [TestClass]
+    public class WordConcatenation_Test
+    {
+
+        [TestMethod]
+        [DataRow("catfoxcat", new[] { "cat", "fox" }, new[] { 0, 3 })]
+        [DataRow("catcatfoxfox", new[] { "cat", "fox" }, new[] { 3 })]
+        public void Test(String str, String[] words, int[] expectedSubstringBegining)
+        {
+            WordConcatenation.findWordConcatenation(str, words)
+                .ShouldBe(expectedSubstringBegining);
+        }
+    }
+}
---
+++ CodingPatterns/CodingPatterns/TwoPointers/PairWithTargetSum.cs
@@ -0,0 +1,47 @@
+﻿using Shouldly;
+
+namespace CodingPatterns.TwoPointers
+{
+    [TestClass]
+    public class PairWithTargetSum_Test
+    {
+        [TestMethod]
+        [DataRow(new[] { 1, 2, 3, 4, 6 }, 6, new[] { 1, 3 })]
+        [DataRow(new[] { 2, 5, 9, 11 }, 11, new[] { 0, 2 })]
+        public void Test(int[] arr, int targetSum, int[] result)
+        {
+            PairWithTargetSum.search(arr, targetSum)
+                .ShouldBe(result);
+        }
+    }
+    class PairWithTargetSum
+    {
+        public static int[] search(int[] arr, int targetSum)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            while (left < right)
+            {
+
+                var sum = arr[left] + arr[right];
+
+
+                if (sum > targetSum)
+                {
+                    right--;
+                    continue;
+                }
+                else if (sum < targetSum)
+                {
+                    left++;
+                    continue;
+                }
+
+
+                return new int[] {left, right};
+            }
+            // TODO: Write your code here
+            return new int[] { -1, -1 };
+        }
+    }
+}
