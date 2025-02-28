d7d8f10696b9cd403b482f9d50eedf6fd5776453
admin <admin@MACHINE>
2025-02-28 17:05:53 +0200

    RemoveDuplicates

--- CodingPatterns/CodingPatterns/TwoPointers/RemoveDuplicates.cs
+++ CodingPatterns/CodingPatterns/TwoPointers/RemoveDuplicates.cs
@@ -5,6 +5,8 @@ namespace CodingPatterns.TwoPointers
     [TestClass]
     public class RemoveDuplicates_Test
     {
+
+
         [TestMethod]
         [DataRow(new int[] { 2, 3, 3, 3, 6, 9, 9 }, 4)]
         [DataRow(new int[] { 2, 2, 2, 11 }, 2)]
@@ -14,10 +16,38 @@ namespace CodingPatterns.TwoPointers
             RemoveDuplicates.remove(arr)
                 .ShouldBe(result);
         }
+
+        [TestMethod]
+        [DataRow(new int[] { 3, 2, 3, 6, 3, 10, 9, 3 }, 3, 4)]
+        [DataRow(new int[] { 2, 11, 2, 2, 1 }, 2, 2)]
+
+        public void Test2(int[] arr, int key, int result)
+        {
+            RemoveDuplicates.remove(arr, key)
+                .ShouldBe(result);
+        }
     }
 
     class RemoveDuplicates
     {
+        public static int remove(int[] arr, int key)
+        {
+            int left = 0;
+
+            for (int right = 0; right < arr.Length; right++)
+            {
+                var rValue = arr[right];
+
+                if (rValue != key)
+                {
+                    arr[left] = arr[right];
+                    left++;
+                }
+            }
+
+
+            return left ;
+        }
 
         public static int remove(int[] arr)
         {
